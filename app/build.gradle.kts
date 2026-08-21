2aeebe254a2abc1998d200dcee10276f9f484ffd
Pradeep Kumar <pradeep.kumar1@gendigital.com>
2026-08-21 22:59:39 +0530

    build: advance Android version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         applicationId = "com.kabi.pillpal.meditick"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         buildConfigField("String", "AI_SCAN_ENDPOINT", quotedBuildConfig(aiScanEndpoint))
